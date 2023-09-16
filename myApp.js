f63f47eace77b5b3356ca07a4d3a02650fee62fa
Adam Richardson <mystormball@gmail.com>
2023-09-16 10:25:12 -0400

    Disable iFraming

--- myApp.js
+++ myApp.js
@@ -11,6 +11,7 @@ app.use("/_api", api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/views/index.html");
 });
+app.use(helmet({ xFrameOptions: { action: "deny" } }));
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
