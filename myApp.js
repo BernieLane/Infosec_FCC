e88b986c778176ee0f8d502861a1ceb96f3deffd
BernieLane <51051814+BernieLane@users.noreply.github.com>
2023-09-16 14:37:21 +0000

    Update disable framing

--- myApp.js
+++ myApp.js
@@ -6,16 +6,16 @@ module.exports = app;
 const api = require("./server.js");
 app.use(express.static("public"));
 app.use(helmet.hidePoweredBy());
-app.disable("strict-transport-security");
-app.use("/_api", api);
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/views/index.html");
-});
 app.use(
   helmet.frameguard({
     action: 'deny'
   })
 );
+app.disable("strict-transport-security");
+app.use("/_api", api);
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + "/views/index.html");
+});
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
