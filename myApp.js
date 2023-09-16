85bd6dffbfd7113e2bd9ad1d3f6935cd603ea03d
Adam Richardson <mystormball@gmail.com>
2023-09-16 10:13:15 -0400

    Update Express header

--- myApp.js
+++ myApp.js
@@ -5,12 +5,12 @@ const helmet = require("helmet");
 module.exports = app;
 const api = require("./server.js");
 app.use(express.static("public"));
+app.use(helmet.hidePoweredBy());
 app.disable("strict-transport-security");
 app.use("/_api", api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/views/index.html");
 });
-helmet.hidePoweredBy();
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
